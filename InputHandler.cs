5b86c7e20dd434eb7dd0b9275dbcac204fd2e34d
MartianRanger <35358754+MartianRanger@users.noreply.github.com>
2019-10-11 11:01:34 -0600

    Weighted Sum
    Used in Unity project to store multiple inputs and find the weighted sum of them all

---
+++ InputHandler.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public static class InputHandler
+{
+
+    public static string processText(string inS)
+    {
+       return inS;
+    }
+    public static int getAverageChars(string inS)
+    {
+        string[] words = inS.Split(' ');
+        int sum = 0;
+        foreach (var word in words)
+        {
+            sum += word.Length;
+        }
+        return sum / words.Length;
+    }
+    public static double[] parseArray(string doubleString)
+    {
+        string[] mathString = doubleString.Split(' ');
+        double[] arlet = new double[mathString.Length];
+
+        int i = 0;
+        foreach (var word in mathString)
+        {
+            arlet[i] = getNum(mathString[i]);
+            i++;
+        }
+        return arlet;
+    }
+    
+    public static double processText(double[] values, double percent)
+    {
+        /*Debug.Log("Before input handler: " + inS);
+        //this.processedText = inS;
+        this.processedText = InputHandler.processText(inS);*/
+
+        double sum = 0;
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+        double newValue = percent * (sum / values.Length);
+
+        return newValue;
+        //displayOutput();
+    }
+
+    public static double getNum(string iS)
+    {
+        try
+        {
+            double num = Double.Parse(iS);
+            return num;
+        }
+        catch (FormatException e)
+        {
+            Debug.Log(e);
+            return -1;
+        }
+    }
+
+    public static bool isNum(string inS)
+    {
+        try
+        {
+            double num = Double.Parse(inS);
+            return true;
+        }
+        catch (FormatException e)
+        {
+            Debug.Log(e);
+            return false;
+        }
+    }
+    public static bool betterIsNum(string inS)
+    {
+        if (Double.TryParse(inS, out double j))
+            return true;
+        else
+            return false;
+
+    }
+}
---
+++ UILogic.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class UILogic : MonoBehaviour
+{
+    public Text text;
+    public string processedText;
+    public InputField input1;
+    public InputField input2;
+    public InputField input3;
+    public InputField percent1;
+    public InputField percent2;
+    public InputField percent3;
+
+    public double[] grades1;
+    public double[] grades2;
+    public double[] grades3;
+
+    public double weight1;
+    public double weight2;
+    public double weight3;
+
+    public double answer;
+    public void getInput()
+    {
+        grades1 = InputHandler.parseArray(input1.text);
+        grades2 = InputHandler.parseArray(input2.text);
+        grades3 = InputHandler.parseArray(input3.text);
+
+        weight1 = InputHandler.getNum(percent1.text);
+        weight2 = InputHandler.getNum(percent2.text);
+        weight3 = InputHandler.getNum(percent3.text);
+
+        displayOutput();
+    }
+    public void displayOutput()
+    {
+        double grade1 = InputHandler.processText(grades1, weight1);
+        double grade2 = InputHandler.processText(grades2, weight2);
+        double grade3 = InputHandler.processText(grades3, weight3);
+        double totalPercent = weight1 + weight2 + weight3;
+        answer = (grade1 + grade2 + grade3) / totalPercent;
+        /*Debug.Log(processedText);
+        string typeOfInput = "none";
+        if (InputHandler.betterIsNum(processedText))
+        {
+            typeOfInput = "number";
+        }
+        else
+        {
+            typeOfInput = "string";
+        }*/
+        text.text = answer.ToString();
+        //text.text = InputHandler.getAverageChars(processedText).ToString();
+    }
+    /*public static double processText(double[] values, double percent)
+    {
+        /*Debug.Log("Before input handler: " + inS);
+        //this.processedText = inS;
+        this.processedText = InputHandler.processText(inS);
+
+        double sum = 0;
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+        double newValue = percent * (sum / values.Length);
+
+        return newValue;
+        //displayOutput();
+    }*/
+
+}
